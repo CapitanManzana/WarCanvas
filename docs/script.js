036502d1f910358c6a8f222422c519115af4d373
Ricardo Luy <130751044+rluy02@users.noreply.github.com>
2025-10-02 23:00:52 +0200

    pag web

---
+++ docs/script.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const containers = document.querySelectorAll('.card-container');
+    // Función para voltear cartas
+    containers.forEach(container => {
+        container.addEventListener('click', (e) => {
+            const card = e.target.closest('.card');
+            if (card) {
+                card.classList.toggle('flipped');
+            }
+        });
+    });
+
+    // Botones, enlaces y logo
+    const logoHeader = document.querySelector('.logo-header');
+    const btnInicio = document.getElementById('btn-inicio');
+    const btnReglas = document.getElementById('btn-reglas');
+    const btnCreditos = document.getElementById('btn-creditos');
+
+    // Secciones (objeto)
+    const secciones = {
+        inicio: document.getElementById('inicio'),
+        reglas: document.getElementById('reglas'),
+        creditos: document.getElementById('creditos')
+    };
+
+    // Función para mostrar una sección y ocultar todas las demás
+    function mostrarSeccion(seccionMostrar) {
+        for (const key in secciones) {
+            if (secciones[key]) {
+                secciones[key].style.display = (secciones[key] === seccionMostrar) ? 'block' : 'none';
+            }
+        }
+    }
+
+    // Eventos
+    btnInicio.addEventListener('click', e => {
+        e.preventDefault();
+        mostrarSeccion(secciones.inicio);
+    });
+
+    logoHeader.addEventListener('click', e => {
+        e.preventDefault();
+        mostrarSeccion(secciones.inicio);
+    });
+
+    btnReglas.addEventListener('click', e => {
+        e.preventDefault();
+        mostrarSeccion(secciones.reglas);
+    });
+
+    btnCreditos.addEventListener('click', e => {
+        e.preventDefault();
+        mostrarSeccion(secciones.creditos);
+    });
+});
